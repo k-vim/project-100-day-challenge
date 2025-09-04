9928c507a59f12b1dd2785282ac8be6ccdc75e05
k-vimla <vimlakushwaha56@gmail.com>
2025-09-04 23:59:23 +0530

    done

--- day-1/first.py
+++ day-1/first.py
@@ -3,5 +3,5 @@
 # to be printed on console or output screen
 #for example i am printiing my name .. as below
 print("Vimla Kushwaha")
-
+print("4-sept-2025")
 # and when i will run this program -- it will reflect as Vimla Kushwaha on the output screen 
