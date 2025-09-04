d9d7492dc0304bde066d4d87da9d0bb888a5504c
k-vimla <vimlakushwaha56@gmail.com>
2025-09-04 23:57:19 +0530

    first commit

---
+++ day-1/first.py
@@ -0,0 +1,7 @@
+# a tiny start 
+#when we use print function .. we indicate the terminal to print everything in the parenthesis 
+# to be printed on console or output screen
+#for example i am printiing my name .. as below
+print("Vimla Kushwaha")
+
+# and when i will run this program -- it will reflect as Vimla Kushwaha on the output screen 
